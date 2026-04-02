b9d8387a34db1d5e2325b5637f84e31b80d8ac0c
Aryan Singh <aryansingh920@outlook.com>
2026-04-03 00:24:02 +0530

    feat: update Terraform state with Kubernetes deployment for Nginx
    - Increased serial number from 2 to 5 in the Terraform state backup.
    - Added a new Kubernetes manifest resource for an Nginx deployment.
    - Configured deployment with 2 replicas, resource limits, and requests.
    - Specified container image version as nginx:1.14.2.
    - Defined metadata, labels, and specifications for the deployment.

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ cd terraform
 terraform init   
 terraform apply
 
-
+terraform destroy
 
 # You can also port-forward the whole deployment without picking a pod:
 kubectl port-forward deployment/nginx-deployment 8080:80 -n terraform-lab
@@ -22,3 +22,13 @@ kubectl logs -l app=nginx -n terraform-lab
 
 # Enter the first pod it finds with the label app=nginx
 kubectl exec -it $(kubectl get pods -n terraform-lab -l app=nginx -o name | head -n 1) -n terraform-lab -- /bin/bash
+
+
+# Replace with one of your actual pod names
+kubectl delete pod nginx-deployment-c845f45b5-fghpd -n terraform-lab
+
+
+
+kubectl get pods -n terraform-lab
+
+kubectl get ns terraform-lab      
